4a769eacab2e841db6f0522bc46055b589cda574
Pilisa Gqokoma <60602000+Pilisa@users.noreply.github.com>
2026-05-05 10:42:47 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,21 +1,32 @@
 CREATE TABLE Books (
     BookID INT PRIMARY KEY,
-    Title VARCHAR(100),
-    Author VARCHAR(100),
+    Title VARCHAR(100) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
     Genre VARCHAR(50),
-    Available BOOLEAN
+    Available BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE Members (
     MemberID INT PRIMARY KEY,
-    FullName VARCHAR(100),
-    Email VARCHAR(100)
+    FullName VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE BorrowedBooks (
     BorrowID INT PRIMARY KEY,
     MemberID INT,
     BookID INT,
-    BorrowDate DATE,
-    ReturnDate DATE
+    BorrowDate DATE NOT NULL,
+    ReturnDate DATE,
+
+    -- Foreign Keys (RELATIONSHIPS)
+    CONSTRAINT fk_member
+        FOREIGN KEY (MemberID)
+        REFERENCES Members(MemberID)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_book
+        FOREIGN KEY (BookID)
+        REFERENCES Books(BookID)
+        ON DELETE CASCADE
 );
