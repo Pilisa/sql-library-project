4786e5c3699cd32eeeee5bb8b8cebd8c66db9365
Pilisa Gqokoma <60602000+Pilisa@users.noreply.github.com>
2026-05-05 10:31:09 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,3 +10,8 @@ SELECT Members.FullName, Books.Title, BorrowedBooks.BorrowDate
 FROM BorrowedBooks
 JOIN Members ON BorrowedBooks.MemberID = Members.MemberID
 JOIN Books ON BorrowedBooks.BookID = Books.BookID;
+
+-- Book with ID 2 is now borrowed → should not be available
+UPDATE Books
+SET Available = FALSE
+WHERE BookID = 2;
