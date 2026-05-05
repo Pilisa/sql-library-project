3205ebfccaa1662589e5dccfbad93924c06d48a2
Pilisa Gqokoma <60602000+Pilisa@users.noreply.github.com>
2026-05-05 10:40:24 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -15,3 +15,26 @@ JOIN Books ON BorrowedBooks.BookID = Books.BookID;
 UPDATE Books
 SET Available = FALSE
 WHERE BookID = 2;
+
+SELECT * FROM Books WHERE BookID = 2;
+SELECT * FROM Members WHERE MemberID = 1;
+
+-- Member returns the book
+UPDATE BorrowedBooks
+SET ReturnDate = '2026-05-05'
+WHERE BorrowID = 1;
+SELECT * FROM BorrowedBooks WHERE BorrowID = 1;
+
+-- Delete a member
+DELETE FROM Members
+WHERE MemberID = 2;
+SELECT * FROM Members;
+
+DELETE FROM Books
+WHERE BookID = 3;
+
+SELECT * FROM Books;
+
+--This deletes EVERYTHING
+DELETE FROM BorrowedBooks;
+SELECT * FROM BorrowedBooks;
